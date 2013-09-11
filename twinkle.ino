77414d03a03d985bee3c045ba55ce036c46c67c8
jjinno <jeremiah.jinno@gmail.com>
2013-09-10 23:46:44 -0700

    fixed
    fixed the random twinkles... now they work ;)

--- twinkle.ino
+++ twinkle.ino
@@ -1,22 +1,26 @@
 #include "SPI.h"
 #include "Adafruit_WS2801.h"
 
-#define UNINITIALIZED    -1  // This is how we identify an uninitialized "twinkle"
-#define LED_STRIP_LENGTH 16  // The total number of LED pixels
+#define UNINITIALIZED    -1  // This is how we identify an uninitialized pixel
+#define LED_STRIP_LENGTH 18   // The total number of LED pixels
 #define TOTAL_TWINKLES   4   // The number of LED pixels that will twinkle at once
 
-uint8_t dataPin  = 13;       // Yellow wire on Adafruit Pixels
-uint8_t clockPin = 12;       // Green wire on Adafruit Pixels
+#define dataPin    13        // Yellow wire on Adafruit Pixels
+#define clockPin   12        // Green wire on Adafruit Pixels
 Adafruit_WS2801 strip = Adafruit_WS2801(LED_STRIP_LENGTH, dataPin, clockPin);
 
 struct Twinkle
 {
   int index;
-  int brightness;         // has to be an int so we can go < 0, but also up to 255
-  uint32_t color;
   
-  char stepSize;          // min:-128, max:127
-  uint8_t maxBrightness;  // min:0, max: 255
+  uint8_t r;
+  uint8_t g;
+  uint8_t b;
+  uint32_t color;         // What we are heading for...
+  
+  int denominator;        // The magic number we are dividing by
+  int numerator;          // where we are in the multiplication
+  char direction;         // the direction we are headed
 };
 
 Twinkle twinkles[TOTAL_TWINKLES];
@@ -35,8 +39,8 @@ uint32_t Color(byte r, byte g, byte b)
 
 void initTwinkle()
 {
-  uint8_t i=0, j=0;
-  int newRandom=0;
+  uint8_t i=0, j=0, m=0;
+  int led=0;
   
   for (i=0; i<TOTAL_TWINKLES; i++) {
     
@@ -48,23 +52,30 @@ void initTwinkle()
     if (twinkles[i].index != UNINITIALIZED) continue;  
     
 pickNewRandom:
-    newRandom = random(LED_STRIP_LENGTH);
+    led = random(LED_STRIP_LENGTH);
     
     // Is that number already used?
     for (j=0; j<TOTAL_TWINKLES; j++) {
-      if (twinkles[j].index == newRandom) {
+      if (twinkles[j].index == led) {
         goto pickNewRandom;
       }
     }
     
     // Give the next LED that new value
-    twinkles[i].index = newRandom;
-    // Pick a random color
-    twinkles[i].color = Color(random(256), random(256), random(256));
-    // Pick a random stepSize
-    twinkles[i].stepSize = random(10);
-    // Pick a random maxBrightness
-    twinkles[i].maxBrightness = random(255 - (twinkles[i].stepSize + 1));
+    twinkles[i].index = led;
+    
+    // Pick a random END color...
+    twinkles[i].r = random(256);
+    twinkles[i].g = random(256);
+    twinkles[i].b = random(256);
+
+    // And the color we display each time...
+    twinkles[i].color = Color(0,0,0);
+    
+    // Pick a percentage of brightness we are at now...
+    twinkles[i].denominator = random(100,255);
+    twinkles[i].numerator = 0;
+    twinkles[i].direction = random(16);
   }
 }
 
@@ -73,40 +84,81 @@ void resetTwinkles()
   uint8_t i=0;
   for (i=0; i<TOTAL_TWINKLES; i++) {
     twinkles[i].index = UNINITIALIZED;
+    twinkles[i].r = 0;
+    twinkles[i].g = 0;
+    twinkles[i].b = 0;
+    twinkles[i].numerator = 0;
+    twinkles[i].direction = 1;
   }
   
-  // Initialize all uninitialized twinkles... which at this point is
-  // all of them...
+  // Initialize all uninitialized twinkles... 
+  // which at this point is all of them...
   initTwinkle();
 }
 
 void displayTwinkles(uint8_t wait)
 {
-  uint8_t i=0;
-  for (i=0; i<TOTAL_TWINKLES; i++) {
-    if (twinkles[i].index == UNINITIALIZED) continue;
-    strip.setPixelColor(twinkles[i].index, twinkles[i].color);
+  uint8_t i=0,j=0;
+  boolean set = false;
+  for (i=0; i<LED_STRIP_LENGTH; i++) {
+    
+    // If it was one of the Twinkles, set it here
+    for (j=0; j<TOTAL_TWINKLES; j++) {
+      if (twinkles[j].index != i) continue;
+      strip.setPixelColor(i, twinkles[j].color); 
+      set = true;
+      break;
+    }
+    
+    // If it was not one of the Twinkles, clear it
+    if (set == false) {
+      strip.setPixelColor(i, 0);
+    }
+    
+    // Clear the value
+    set = false;
   }
+  
   strip.show();
   delay(wait);
 }
 
 void stepTwinkles()
 {
-  uint8_t i=0;
-  for (i=0; i<TOTAL_TWINKLES; i++) {
-    twinkles[i].brightness += twinkles[i].stepSize;    
-    if (twinkles[i].brightness > twinkles[i].maxBrightness) {
-      twinkles[i].stepSize = -1 * twinkles[i].stepSize;
-    }
-    if (twinkles[i].brightness < 0) {
-      twinkles[i].index = UNINITIALIZED;
-      initTwinkle(); // Start another random twinkle...
+  uint8_t i=0,j=0;
+  for (i=0; i<LED_STRIP_LENGTH; i++) {
+    for (j=0; j<TOTAL_TWINKLES; j++) {
+      if (twinkles[j].index != i) continue;
+      
+      // set the color...
+      twinkles[j].color = Color(
+        (byte)(((double)twinkles[j].r)*((double)twinkles[j].numerator / (double)twinkles[j].denominator)),
+        (byte)(((double)twinkles[j].g)*((double)twinkles[j].numerator / (double)twinkles[j].denominator)),
+        (byte)(((double)twinkles[j].b)*((double)twinkles[j].numerator / (double)twinkles[j].denominator))
+      );
+      
+      // add 'd' to 'y' until it reaches 'x' or '0'...
+      twinkles[j].numerator += twinkles[j].direction;
+      if (twinkles[j].numerator >= twinkles[j].denominator) {
+        twinkles[j].numerator = twinkles[j].denominator;
+        twinkles[j].direction = -1 * twinkles[j].direction;
+      }
+      else
+      if (twinkles[j].numerator <= 0) {
+        twinkles[j].numerator = 0;
+        twinkles[j].direction = 1;
+        twinkles[j].index = UNINITIALIZED;
+        initTwinkle();
+      }
+      
+      // If we got here it was because index == i, so we can break
+      break;
     }
   }
 }
 
 void setup() { 
+  Serial.begin(19200);
   strip.begin();
   strip.show();
   
