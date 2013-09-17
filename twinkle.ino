fbb0e82f5fb86dc5ad422c5654706e77b467ecd1
jjinno <jeremiah.jinno@gmail.com>
2013-09-17 14:45:30 -0700

    Update to include second pin read
    This update includes a second pin read to incorporate use of a second main-loop functionality based upon a second switch. Input-1 still is responsible for initializing the Twinkles, but input-2 is now capable of stealing the entire main loop for a seperate sequence (in this case setting all LEDs to blue on WS2801 strips).

--- twinkle.ino
+++ twinkle.ino
@@ -1,13 +1,26 @@
 #include "SPI.h"
+
+#define __WS2801
+
+#ifdef __WS2801
 #include "Adafruit_WS2801.h"
+#else
+#include <Adafruit_NeoPixel.h>
+#endif
 
-#define UNINITIALIZED    -1  // This is how we identify an uninitialized pixel
-#define LED_STRIP_LENGTH 18   // The total number of LED pixels
-#define TOTAL_TWINKLES   4   // The number of LED pixels that will twinkle at once
+#define UNINITIALIZED    -1   // This is how we identify an uninitialized pixel
+#define LED_STRIP_LENGTH 42   // The total number of LED pixels
+#define TOTAL_TWINKLES   10   // The number of LED pixels that will twinkle at once
+#define INPUT1_PIN       8    // A pin that is attached to the accelerometer
+#define INPUT2_PIN       9    // A pin that is attached to the accelerometer
+#define DATA_PIN         13   // Data wire for pixels (both WS2801 & WS2811 style)
 
-#define dataPin    13        // Yellow wire on Adafruit Pixels
-#define clockPin   12        // Green wire on Adafruit Pixels
-Adafruit_WS2801 strip = Adafruit_WS2801(LED_STRIP_LENGTH, dataPin, clockPin);
+#ifdef __WS2801
+#define CLOCK_PIN         12   // Clock wire for WS2801-style pixels only
+Adafruit_WS2801 strip = Adafruit_WS2801(LED_STRIP_LENGTH, DATA_PIN, CLOCK_PIN);
+#else
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_STRIP_LENGTH, DATA_PIN, NEO_GRB + NEO_KHZ800);
+#endif
 
 struct Twinkle
 {
@@ -20,7 +33,7 @@ struct Twinkle
   
   int denominator;        // The magic number we are dividing by
   int numerator;          // where we are in the multiplication
-  char direction;         // the direction we are headed
+  char direction;         // the direction we are headed (brighter/dimmer)
 };
 
 Twinkle twinkles[TOTAL_TWINKLES];
@@ -41,6 +54,10 @@ void initTwinkle()
 {
   uint8_t i=0, j=0, m=0;
   int led=0;
+
+  if (digitalRead(INPUT1_PIN) != HIGH) {
+    return;
+  }
   
   for (i=0; i<TOTAL_TWINKLES; i++) {
     
@@ -82,6 +99,7 @@ pickNewRandom:
 void resetTwinkles()
 {
   uint8_t i=0;
+  
   for (i=0; i<TOTAL_TWINKLES; i++) {
     twinkles[i].index = UNINITIALIZED;
     twinkles[i].r = 0;
@@ -100,6 +118,7 @@ void displayTwinkles(uint8_t wait)
 {
   uint8_t i=0,j=0;
   boolean set = false;
+
   for (i=0; i<LED_STRIP_LENGTH; i++) {
     
     // If it was one of the Twinkles, set it here
@@ -126,6 +145,7 @@ void displayTwinkles(uint8_t wait)
 void stepTwinkles()
 {
   uint8_t i=0,j=0;
+ 
   for (i=0; i<LED_STRIP_LENGTH; i++) {
     for (j=0; j<TOTAL_TWINKLES; j++) {
       if (twinkles[j].index != i) continue;
@@ -148,7 +168,6 @@ void stepTwinkles()
         twinkles[j].numerator = 0;
         twinkles[j].direction = 1;
         twinkles[j].index = UNINITIALIZED;
-        initTwinkle();
       }
       
       // If we got here it was because index == i, so we can break
@@ -161,12 +180,23 @@ void setup() {
   Serial.begin(19200);
   strip.begin();
   strip.show();
-  
+
+  pinMode(INPUT1_PIN, INPUT);
+  pinMode(INPUT2_PIN, INPUT);
   randomSeed(analogRead(0));
   resetTwinkles();
 }
 
 void loop() {
-  displayTwinkles(10);
-  stepTwinkles();
+  if (digitalRead(INPUT2_PIN) != HIGH) {
+    displayTwinkles(10);
+    stepTwinkles();
+    initTwinkle();
+  }
+  else {
+    for (int i=0; i<LED_STRIP_LENGTH; i++) {
+      strip.setPixelColor(i, Color(255,0,0));
+    }
+    strip.show();
+  }
 }
